38708883f332050bc66adbaab781b6973f5a520a
s30084pjatk <s30084@pjwstk.edu.pl>
2026-03-16 14:00:35 +0100

    dorobiony generator ajdików

---
+++ Main/GeneratorAjdikow.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1;
+
+public class GeneratorAjdikow
+{
+    private static int lastDeviceId;
+    private static int lastPersonId;
+    private static int lastRentalId;
+
+    public static int LastDeviceId() {
+        return ++lastDeviceId;
+    }
+    public static int LastPersonId() {
+        return ++lastPersonId;
+    }
+    public static int LastRentalId() {
+        return ++lastRentalId;
+    }
+
+}
