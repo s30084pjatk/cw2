b81a130768f5cb39c61957c8d378ac77a3716d17
s30084pjatk <s30084@pjwstk.edu.pl>
2026-03-18 12:11:50 +0100

    metoda Service.zwroc Nietestowana jeszcze. Dołożenie atrybutu dostepnosci sprzętu
    Pierwsze testy w Main.

--- Main/Device.cs
+++ Main/Device.cs
@@ -8,8 +8,21 @@ public abstract class Device
         Nazwa = nazwa;
         Dostepnosc = dostepnosc;
     }
-    
+
     public int Id { get; set; }
     public string Nazwa { get; set; }
     public bool Dostepnosc { get; set; }
+
+    public bool CzyWypozyczony()
+    {
+        foreach (Wypozyczenie wyp in Service.Rentals)
+        {
+            if (wyp.Sprzet == this && wyp.FaktycznaDataZwrotu == null)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
