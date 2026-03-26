fa48142f984de64aaee25c33fd677dde5c05430c
s30084pjatk <s30084@pjwstk.edu.pl>
2026-03-26 12:33:59 +0100

    Final_v2

--- Main/Modules/Devices/Device_parent.cs
+++ Main/Modules/Devices/Device_parent.cs
@@ -12,17 +12,4 @@ public abstract class Device_parent
     public int Id { get; set; }
     public string Nazwa { get; set; }
     public bool Dostepnosc { get; set; }
-
-    public bool CzyWypozyczony()
-    {
-        foreach (Wypozyczenie wyp in Service.Rentals)
-        {
-            if (wyp.Sprzet == this && wyp.FaktycznaDataZwrotu == null)
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
 }
